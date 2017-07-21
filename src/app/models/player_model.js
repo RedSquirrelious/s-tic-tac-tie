56fdc0e51c25ff8dc25e1b0296a3ddba0080ebed
RedSquirrelious <redsquirrelious@gmail.com>
2017-07-20 22:11:46 -0700

    add lists of names and marks to player model

--- src/app/models/player_model.js
+++ src/app/models/player_model.js
@@ -24,6 +24,34 @@ const Player = Backbone.Model.extend({
 
   playSpace: function(space) {
     space.mark = this.mark;
+  },
+
+  markImages: {
+    grass: 'images/squirrel-grass.jpg',
+    rocks: 'images/squirrel-rocks.jpg',
+    snow: 'images/squirrel-snow.jpg',
+    bugs: 'images/Bugs_Bunny.jpg',
+    elmer: 'images/Elmer_Fudd.jpg',
+    sam: 'images/Yosemite_Sam.jpg',
+    daffy: 'images/Daffy_Duck.jpg',
+    porky: 'images/Porky_Pig.jpg',
+    marvin: 'images/Marvin_the_Martian.jpg',
+    coyote: 'images/Wile_E._Coyote.jpg',
+    roadrunner: 'images/Roadrunner.jpg'
+  },
+
+  names: {
+    grass: 'Park Squirrel',
+    rocks: 'Rock Squirrel',
+    snow: 'Snow Squirrel',
+    bugs: 'Bugs Bunny',
+    elmer: 'Elmer Fudd',
+    sam: 'Yosemite Sam',
+    daffy: 'Daffy Duck',
+    porky: 'Porky Pig',
+    marvin: 'Marvin the Martian',
+    coyote: 'Wile E. Coyote',
+    roadrunner: 'Roadrunner'
   }
 
 }); 
